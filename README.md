1987cfbe8ec23f912c028c3fe77f948fa97ec750
ThomasCza <czarnecki.thomas@orange.fr>
2022-08-13 13:22:39 +0200

    Tirage au sort
    ok

---
+++ README.md
@@ -0,0 +1,3 @@
+# Roulette
+
+Tirage au sort pour faire 5 groupes de 3 personnes dans la promo.
---
+++ script/app.js
@@ -0,0 +1,27 @@
+var participants = ["Marion", "Timeo", "Theo", "Alix","Carole","Eric","David","Rudy","Ylann",
+ "Marie","Benoît","Nicolas","Laetitia","Coralie","Christelle","Thomas","Amandine", "Tristan","Aaron","Elina","Laura","Melanie"
+];
+
+const main = document.querySelector('main');
+const button = document.querySelector('button');
+
+button.addEventListener('click', createGroup);
+
+
+function createGroup () {
+
+	for (var i = 0; i < 10; i++) {
+		gagnants = document.querySelector("#test");
+		var index = Math.floor(Math.random() * participants.length);
+		var participant = participants[index];
+		participants = participants.slice(0, index).concat(participants.slice(index+1, participants.length))
+		console.log(participant)
+		participantList = document.createElement("li")
+		participantList.innerHTML = participant
+
+		gagnants.appendChild(participantList);
+		// myInterval = setInterval(createGroup, 3000);
+	}
+
+}
+
