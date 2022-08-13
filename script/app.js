db888e80df97e5f0d6a4e7cf9c4ff0ebf3d33dbb
ThomasCza <czarnecki.thomas@orange.fr>
2022-08-13 21:07:56 +0200

    Tirage sort Jeux MLM
    ok

--- script/app.js
+++ script/app.js
@@ -1,27 +1,42 @@
 //tirage au sort
 
 var participants = ["Marion", "Timeo", "Theo", "Alix","Carole","Eric","David","Rudy","Ylann",
- "Marie","Benoît","Nicolas","Laetitia","Coralie","Christelle","Thomas","Amandine", "Tristan","Aaron","Elina","Laura","Melanie"
+ "Marie","Benoit","Nicolas","Laetitia","Coralie","Christelle","Thomas","Amandine", "Tristan","Aaron","Elina","Laura","Melanie"
 ];
 
 const button = document.querySelector('button');
+const gagnants = document.querySelector('#test');
+
 
 button.addEventListener('click', createGroup);
+setTimeout(createGroup, 2000)
 
+function draw () 
+{
+	let index = Math.floor(Math.random() * participants.length);
+	let participant = participants[index];
+	participants = participants.slice(0, index).concat(participants.slice(index + 1, participants.length));
+	participantList = document.createElement("li");
+	participantList.innerHTML = participant;
 
-function createGroup () {
+	gagnants.appendChild(participantList);
+	participantList.style.opacity = '1';
+}
 
-	for (var i = 0; i < 10; i++) {
-		gagnants = document.querySelector("#test");
-		var index = Math.floor(Math.random() * participants.length);
-		var participant = participants[index];
-		participants = participants.slice(0, index).concat(participants.slice(index+1, participants.length))
-		console.log(participant)
-		participantList = document.createElement("li")
-		participantList.innerHTML = participant
-
-		gagnants.appendChild(participantList);
-		
+let i = 1;
+function check ()
+{
+	if (i === 10) {
+		clearInterval(drawInterval);
 	}
+}
 
+function createGroup ()
+{
+	draw()
+	drawInterval = setInterval(() => {
+		draw();
+		i++;
+		check();
+	}, 3000);
 }
